436f9a3d6ec785956f9604fa2107e7f9d88a7c48
Javier Rodriguez <javierorodriguez@gmail.com>
2016-03-18 23:52:30 +0300

    Show action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,7 @@ class RestaurantsController < ApplicationController
    def index
      @restaurants = Restaurant.all
    end
+   def show
+     @restaurant = Restaurant.find(params[:id])
+   end
 end
