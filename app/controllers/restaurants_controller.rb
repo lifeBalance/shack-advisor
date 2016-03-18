c3ef63d2f6920f80cf8d583b087a35cfebc6241f
Javier Rodriguez <javierorodriguez@gmail.com>
2016-03-19 00:53:05 +0300

    Create CommentList component

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,7 @@ class RestaurantsController < ApplicationController
      @restaurants = Restaurant.all
    end
    def show
-     @restaurant = Restaurant.find(params[:id])
+     @restaurant  = Restaurant.find(params[:id])
+     @comments    = @restaurant.comments
    end
 end
